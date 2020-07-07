e7613b2b459c2be6a0ef4ce0507f815ec4f7275d
Khang Vo <kvo189@gmail.com>
2020-07-06 21:20:05 -0500

    Updates

---
+++ precache-manifest.76198c9039b8ddf18ea4d50c5c7a6449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966f91d788356a6ac8c308d4b6a9188e",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "53ffa9a10edb77b42827",
+    "url": "/calculator/static/css/main.8f470532.chunk.css"
+  },
+  {
+    "revision": "ed34ad91e9020be8ea88",
+    "url": "/calculator/static/js/2.c3788b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.c3788b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ffa9a10edb77b42827",
+    "url": "/calculator/static/js/main.e1004a71.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
